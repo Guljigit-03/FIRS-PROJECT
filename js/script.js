8d6dd830c0747732b7a8b3c964ada298ca36c0c7
Guljigit-03 <dujsonbekovgulzigit@gmail.com>
2021-11-03 12:39:29 +0300

    initial

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+
+    loop: true,
+    slidesPerView:3
+    autoplay: {
+        delay:2000
+
+    }
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
